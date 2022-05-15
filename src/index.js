b0d54a50c36e2841fb15d37b3815dfca00c8e066
jeremy-friesen <jeremy.friesen@ontariotechu.net>
2022-05-14 22:03:56 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3E9CD6',
+    },
+  },
+});
+
 root.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </MuiThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
